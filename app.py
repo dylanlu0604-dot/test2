4625e17531afc0d1a2bda5ec794e6544eff79104
dylanlu0604-dot <dylanlu0604@gmail.com>
2025-09-02 12:03:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -317,7 +317,7 @@ if resulttable1_list:
         if ((pre > 0 and after > 0) or (pre < 0 and after < 0)) and (times > 10) and ((prewin + afterwin > 140) or (prewin + afterwin < 60))
         else '❌ 不是有效訊號'
     )
-    st.markdown(f"### {effectivepart1}")
+    st.markdown(f"#### {effectivepart1}")
 
     # 並排：左表右圖
     col1, col2 = st.columns([1, 1])
@@ -367,7 +367,7 @@ if resulttable2_list:
         if ((pre > 0 and after > 0) or (pre < 0 and after < 0)) and (times > 10) and ((prewin + afterwin > 140) or (prewin + afterwin < 60))
         else '❌ 不是有效訊號'
     )
-    st.markdown(f"### {effectivepart2}")
+    st.markdown(f"#### {effectivepart2}")
 
     # 並排：左表右圖
     col1, col2 = st.columns([1, 1])
